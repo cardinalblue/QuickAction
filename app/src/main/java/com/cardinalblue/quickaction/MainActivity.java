ba6daf6ae9985d79689c2c3a4a00e3ec7f818648
PRADA Hsiung <bear.prada@gmail.com>
2016-09-19 21:47:10 +0800

    remove the useless libraries and code

--- app/src/main/java/com/cardinalblue/quickaction/MainActivity.java
+++ app/src/main/java/com/cardinalblue/quickaction/MainActivity.java
@@ -1,9 +1,7 @@
 package com.cardinalblue.quickaction;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -11,32 +9,8 @@ import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
-import com.cardinalblue.quickaction.app.R;
-
-import org.tensorflow.demo.TensorFlowClassifier;
-
-import java.util.concurrent.Callable;
-
-import bolts.Continuation;
-import bolts.Task;
-import io.bugtags.library.Bugtags;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int NUM_CLASSES = 1001;
-    private static final int INPUT_SIZE = 224;
-    private static final int IMAGE_MEAN = 117;
-    private static final float IMAGE_STD = 1;
-    private static final String INPUT_NAME = "input";
-    private static final String OUTPUT_NAME = "output";
-
-    // private static final String MODEL_FILE = "file:///android_asset/dots.pb";
-    private static final String MODEL_FILE = "file:///android_asset/newspaper.pb";
-
-    private static final String LABEL_FILE =
-        "file:///android_asset/imagenet_comp_graph_label_strings.txt";
-
-    private final TensorFlowClassifier tensorflow = new TensorFlowClassifier();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,32 +30,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 createQuickAction().showAtLocation(300,300);
-                tensorflow.initializeTensorFlow(
-                    getAssets(), MODEL_FILE, LABEL_FILE, NUM_CLASSES, INPUT_SIZE, IMAGE_MEAN, IMAGE_STD,
-                    INPUT_NAME, OUTPUT_NAME);
             }
         });
         findViewById(R.id.btn_02).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 createQuickAction().showAtView(view);
-                Task.callInBackground(new Callable<Bitmap>() {
-                    @Override
-                    public Bitmap call() throws Exception {
-                        Bitmap bm = BitmapFactory.decodeStream(getAssets().open("test_selfie.jpg"));
-                        return tensorflow.styleTransfer(Bitmap.createScaledBitmap(bm, INPUT_SIZE, INPUT_SIZE, false));
-                    }
-                }).continueWith(new Continuation<Bitmap, Void>() {
-                    @Override
-                    public Void then(Task<Bitmap> task) throws Exception {
-                        if (task.isFaulted()) {
-                            Log.d("TEST", task.getError().getMessage());
-                            return null;
-                        }
-                        preview.setImageBitmap(task.getResult());
-                        return null;
-                    }
-                }, Task.UI_THREAD_EXECUTOR);
             }
         });
     }
@@ -102,25 +56,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         return qa;
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Bugtags.onResume(this);
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Bugtags.onPause(this);
-    }
-
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent event) {
-        //Callback 3
-        Bugtags.onDispatchTouchEvent(this, event);
-        return super.dispatchTouchEvent(event);
     }
 }
